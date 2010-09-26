7def6fc5b1c69ccaf6bfec9291a3ec1b78b0141e
Sean Sorrell <seansorrell@gmail.com>
2010-09-26 19:51:38 -0400

    Insert mode

--- spec/vim_parser_spec.rb
+++ spec/vim_parser_spec.rb
@@ -42,4 +42,19 @@ describe VimGrammarParser do
     end
 
   end
+
+  describe "insert mode" do
+    it "emits 'inserted: 'abc'' for 'iabc<ESC>'" do
+      (traverse_pp @parser.parse 'iabc<ESC>').should == ["enter insert mode (i)", "a", "b", "c", "leave insert mode"]
+    end
+
+    it "emits 'enter insert with append (a) for 'aabc<ESC>'" do
+      (traverse_pp @parser.parse 'aabc<ESC>').should == ["enter insert with append (a)", "a", "b", "c", "leave insert mode"]
+    end
+
+    it "should handle insert mode in concert with other commands" do
+
+      (traverse_pp @parser.parse '3jihello<ESC>kdd').should_not be_nil
+    end
+  end
 end
